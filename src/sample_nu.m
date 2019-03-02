b662337237f4b770757bc1279d9430f87a56ea6d
pthouvenin <pierreantoine.thouvenin@gmail.com>
2019-03-02 22:38:11 +0000

    Add webpage

--- src/sample_nu.m
+++
@@ -1,8 +0,0 @@
-function nu_dM = sample_nu(dM1,a_nu,b_nu,L,R)
-%UNTITLED Summary of this function goes here
-%   Detailed explanation goes here
-
-nu_dM = 1./gamrnd(a_nu + L*R/2, 1./bsxfun(@plus,b_nu,0.5*sum(dM1(:).^2))); % [L|R]
-
-end
-
